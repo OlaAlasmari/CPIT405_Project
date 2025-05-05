66a0241c27446e3848067e13a9b0f39e246ae0c5
shouq <shouq.thwap@gmail.com>
2025-05-05 18:11:47 +0300

    edit about-us page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -12,23 +12,20 @@ const AboutUs = () => {
     return (
 
         <div className="row align-items-center">
-            {/* Text on the left */}
             <div className="col-md-5 proverb-card-left" style={{ marginTop: '80px' }}>
-                <h1 className="main2">"ما هو سعوديولوجي؟"</h1>
+                <h1 className="main2">ما هـو سعـوديولـوجي ؟</h1>
                 <h1 className="main7" style={{ marginTop: '2em' }}>
                     نحن منصة تهتم برواية القصص والهوية الثقافية السعودية، ونسعى لتقديمها بأسلوب عصري يجمع بين أصالة الماضي وروح الحاضر، لننقل إرثنا الثقافي للأجيال القادمة بطريقة ملهمة ومبتكرة
                 </h1>
             </div>
-
-            {/* Image on the right */}
             <div className="col-md-6 text-center">
                 <img
-                    src="/img/aboutUs.jpg"
+                    src="/img/aa.png"
                     alt="سعوديولوجي"
                     className="about-image"
                 />
             </div>
-            {/* رسالتنا */}
+
             <section className="mb" data-aos="fade-up">
                 <StoryCard
                     title="رسالتـنا"
@@ -37,10 +34,10 @@ const AboutUs = () => {
                 />
 
             </section>
-            <section className="mb-5" data-aos="fade-up">
-                <div className="container"> {/* ADD THIS LINE */}
-                    <div className="row justify-content-center text-center g-4">
-                        <h1 className="main2">أهدافنا</h1>
+            <section className="mb-3" data-aos="fade-up">
+                <div className="container">
+                    <div className="row justify-content-center text-center">
+                        <h1 className="main2 mb-0">أهدافنا</h1>
 
                         <div className="col-6 col-md-4">
                             <StoryCard quote="الاعتزاز بالهوية السعودية" author="" />
@@ -54,7 +51,7 @@ const AboutUs = () => {
                             <StoryCard quote="مواكبة التطورات الثقافية" author="" />
                         </div>
                     </div>
-                </div> {/* CLOSE CONTAINER */}
+                </div>
             </section>
         </div>
     );
